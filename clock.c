809b5992139d1731e7a4f55e713f7dffd620393e
Georges Boumis <georges.boumis@parrot.com>
2020-04-23 21:34:44 +0200

    better handle options

--- clock.c
+++ clock.c
@@ -1,6 +1,6 @@
 /*
  * clock - print time in various formats
- * Copyright (C) 2012 George Boumis <developer.george.boumis@gmail.com>
+ * Copyright (C) 2012-2020 George Boumis <developer.george.boumis@gmail.com>
  *
  */
 
@@ -9,89 +9,131 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-typedef char*string;
+#include <assert.h>
 
 extern char *optarg;
 extern int optind, opterr, optopt;
 
-static const char *const options = "tboxlh"; /* Possible options */
-static const char *const formatoptions = "l"; /* Possible options */
-static struct tm *loctime = NULL;
-static time_t clocktime;
-static const char *const licence = "clock - print time in various formats (12h)\nCopyright (C) 2012 George Boumis\nThis program comes with ABSOLUTELY NO WARRANTY;\nThis is free software, and you are welcome to redistribute it\nunder certain conditions;";
+/* Options */
+enum {
+	OptionBinary = (1<<0), // shows in binary format
+	OptionHex = (1<<1), // shows in hexadecimal format
+	OptionOctal = (1<<2), // shows in octal format
+	Option12h = (1<<3), // use 12h
+	OptionLongOutput = (1<<4), // use ":" delimiter
+};
+typedef uint8_t OptionFlags;
 
 
-void convertto( const char option, const char format, const struct tm*localtime );
+void convertto(const OptionFlags option, const struct tm *const localtime);
+void usage(const char *const options, const struct tm *const localtime);
+void print_bits(unsigned integer, unsigned bits);
 
 
 int main ( int argc, char *argv[] ) {
-	int option = -1;
-	int format = -1;
-	if (argc>1)
-		option = getopt(argc, argv, options);
+	const char *const options = "botxhfl"; /* Possible options */
+	OptionFlags flags = Option12h;
 
-	time(&clocktime); /* get time frim Epoch */
+	time_t clocktime;
+	time(&clocktime); /* get time from Epoch */
+	struct tm *loctime = NULL;
 	loctime = localtime(&clocktime);
-	loctime->tm_year += 1900; /* Needed because tm_year equals year - 1900 */
-	loctime->tm_hour %= 12;
+	loctime->tm_year += 1900; /* needed because tm_year equals year - 1900 */
 
-	format = getopt(argc, argv, formatoptions);
-#ifdef DEBUG_FLAG
-	printf("option1 : %d\noption2 : %d\n", option, format);
-#endif
-	if ( option == 'h' )
-		puts(licence);
-	else
-		convertto(option, format, loctime);
+	int option;
+	while ((option = getopt(argc, argv, options)) != -1) {
+		switch (option) {
+			case 'h':
+				usage(options, loctime);
+				exit(EXIT_SUCCESS);
+				break;
+			case 'b':
+				flags |= OptionBinary;
+			case 'x':
+				flags |= OptionHex;
+				break;
+			case 'o':
+				flags |= OptionOctal;
+				break;
+			case 'f':
+				flags &= ~Option12h;
+				break;
+			case 'l':
+				flags |= OptionLongOutput;
+				break;
+			case 't':
+				break;
+			default:
+				exit(EXIT_FAILURE);
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (flags & Option12h) {
+		loctime->tm_hour %= 12;
+	}
+
+	convertto(flags, loctime);
 
 	return EXIT_SUCCESS;
 }
 
+void usage(const char *const options, const struct tm *const localtime) {
+	const char *const licence = 
+		"clock - print time in various formats\n"
+		"\nCopyright (C) 2012-%d Georges Boumis\n"
+		"This program comes with ABSOLUTELY NO WARRANTY;\n"
+		"This is free software, and you are welcome to redistribute it\n"
+		"under certain conditions;";
+	const char *const usage = 
+		"Usage: clock [-%s]\n"
+		"\t-h\tshows this message.\n"
+		"\t-t\tdefault. show full date. d/m/yyyy h:m:s\n"
+		"\t-f\tuse 24h instead of 12h. does not work with -b\n"
+		"\t-b\tbinary clock. hhhhh:mmmmmm:ssssss\n"
+		"\t-x\thexadecimal clock. hh:mm:ss\n"
+		"\t-o\toctal clock. hh:mm:ss\n"
+		"\t-l\tlong format. instead of \":\" use newlines.\n";
+	printf(licence, localtime->tm_year);
+	puts("\n");
+	printf(usage, options);
+}
 
-void convertto( const char option, const char format, const struct tm*localtime ) {
+void print_bits(unsigned integer, unsigned bits) {
+	assert(bits > 0);
 	register int i;
-	switch (option) {
-		default:
-		case 'b': /* bonary */
-			{
-				/* Print hours */
-				printf(" ");
-				for (i=(1<<4); i>=1; i>>=1)
-					printf("%d", ((localtime->tm_hour & i) == i) ? 1 : 0);
-				if (format=='l' || option=='l') puts("");
-				else printf(":");
-
-				/* Print minutes */
-				for (i=(1<<5); i>=1; i>>=1)
-					printf("%d", ((localtime->tm_min & i) == i) ? 1 : 0);
-				if (format=='l' || option=='l') puts("\n");
-				else printf(":");
-
-				/* Print seconds */
-				for (i=(1<<5); i>=1; i>>=1)
-					printf("%d", ((localtime->tm_sec& i) == i) ? 1 : 0);
-				puts("");
-				break; 
-			}
-		case 'x': /* hex */
-			if (format=='l')
-				printf("%x\n%2x\n%2x\n", localtime->tm_hour, localtime->tm_min, localtime->tm_sec);
-			else
-				printf("%x:%2x:%2x\n", localtime->tm_hour, localtime->tm_min, localtime->tm_sec);
-			break;
-		case 'o': /* octal */
-			if (format=='l')
-				printf("%2o\n%2o\n%2o\n", localtime->tm_hour, localtime->tm_min, localtime->tm_sec);
-			else
-				printf("%2o:%2o:%2o\n", localtime->tm_hour, localtime->tm_min, localtime->tm_sec);
-			break;
-		case 't': /* */
-			printf("%d/%d/%d %d:%d:%d\n", localtime->tm_mday, localtime->tm_mon, localtime->tm_year, localtime->tm_hour, localtime->tm_min, localtime->tm_sec);
-			break;
+	for (i=(1<<(bits -1)); i>=1; i>>=1) {
+		printf("%d", ((integer & i) == i) ? 1 : 0);
 	}
 }
 
+void convertto(const OptionFlags option, const struct tm *const localtime ) {
+	const char *const delimiter = option & OptionLongOutput 
+		? "\n"
+		: ":";
+	if (option & OptionBinary) {
+		/* Print hours */
+		print_bits(localtime->tm_hour, 4);
+		printf("%s", delimiter);
 
+		/* Print minutes */
+		print_bits(localtime->tm_min, 5);
+		printf("%s", delimiter);
 
+		/* Print seconds */
+		print_bits(localtime->tm_sec, 5);
+		puts("");
+	} else if (option & OptionHex) {
+		printf("%x%s%2x%s%2x\n", localtime->tm_hour, delimiter,
+				localtime->tm_min, delimiter, localtime->tm_sec);
+	} else if (option & OptionOctal) {
+		printf("%2o%s%2o%s%2o\n", localtime->tm_hour, delimiter,
+				localtime->tm_min, delimiter, localtime->tm_sec);
+	} else {
+		printf("%d/%d/%d %d:%d:%d\n", localtime->tm_mday,
+				localtime->tm_mon, localtime->tm_year, localtime->tm_hour,
+				localtime->tm_min, localtime->tm_sec);
+	}
+}
 
