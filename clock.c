3b95b2bab7547b9b092e5656f6469f8155a198d7
Georges Boumis <georges.boumis@parrot.com>
2020-04-23 21:35:56 +0200

    update LICENCE and Copyright

--- clock.c
+++ clock.c
@@ -1,7 +1,27 @@
 /*
  * clock - print time in various formats
- * Copyright (C) 2012-2020 George Boumis <developer.george.boumis@gmail.com>
  *
+ * The MIT License (MIT)
+
+ * Copyright (c) 2012-2020 George Boumis <developer.george.boumis@gmail.com>
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include <stdio.h>
@@ -14,7 +34,6 @@
 extern char *optarg;
 extern int optind, opterr, optopt;
 
-/* Options */
 enum {
 	OptionBinary = (1<<0), // shows in binary format
 	OptionHex = (1<<1), // shows in hexadecimal format
@@ -25,10 +44,9 @@ enum {
 typedef uint8_t OptionFlags;
 
 
-void convertto(const OptionFlags option, const struct tm *const localtime);
 void usage(const char *const options, const struct tm *const localtime);
 void print_bits(unsigned integer, unsigned bits);
-
+void convertto(const OptionFlags option, const struct tm *const localtime);
 
 int main ( int argc, char *argv[] ) {
 	const char *const options = "botxhfl"; /* Possible options */
